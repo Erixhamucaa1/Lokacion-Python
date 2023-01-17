f593bf665c889bc98d80614b7b6a1774a9f9a206
Erixhamucaa1 <122046005+Erixhamucaa1@users.noreply.github.com>
2023-01-17 23:01:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import phonenumbers
 from phonenumbers import geocoder
-phone_number1 = phonenumbers.parse("+355677408989")
+phone_number1 = phonenumbers.parse("...")
 print("\n Phone Numbers Lokacion\n")
 print(geocoder.description_for_number(phone_number1,"en"));
